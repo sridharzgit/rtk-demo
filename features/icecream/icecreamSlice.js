95375f44fcff4ec25c9fc93d2c789b37fd751ac5
vsridhar <vsridhar@cubewise.com>
2022-10-10 15:54:33 +0530

    rtk complete demo using nodejs

---
+++ features/icecream/icecreamSlice.js
@@ -0,0 +1,31 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+const cakeActions = require('../cake/cakeSlice').cakeActions
+
+const initialState = {
+  numOfIcecreams:10
+}
+
+const icecreamSlice = createSlice({
+  name:'icecream',
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIcecreams --
+    }
+  },
+  // extraReducers:{
+  //   ['cake/ordered'] : (state) => {
+  //     state.numOfIcecreams --
+  //   }
+  // }
+  extraReducers: (builder) =>{
+    builder.addCase(cakeActions.ordered,(state)=>{
+      state.numOfIcecreams --
+    })
+
+  }
+
+})
+
+module.exports = icecreamSlice.reducer
+module.exports.icecreamActions = icecreamSlice.actions
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Test")
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
+console.log("Initial state", store.getState())
+const unsubscribe = store.subscribe(()=>{
+  //console.log('updated state',store.getState())
+})
+
+store.dispatch(fetchUsers())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+// //store.dispatch(icecreamActions.restocked(3))
+// store.dispatch(icecreamActions.ordered(1))
+
+//unsubscribe()
